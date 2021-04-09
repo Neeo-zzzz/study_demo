afe2288dd12cf92b87d6bea8d161d5b3074c9905
z <3200105988>
2021-04-09 21:53:13 +0800

    win32第一次提交

--- demo_win32.c
+++ demo_win32.c
@@ -78,8 +78,49 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             GetClientRect(hwnd, &rect);
             DrawText(hdc, TEXT("hhhhhhhhhhhh"), -1, &rect,
                 DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            //DrawTextW()
             EndPaint(hwnd, &ps);
             return 0;
+        case WM_CREATE:
+        {
+            //在WM_CREATE消息中创建子窗口、控件
+            LPCREATESTRUCT psc = (LPCREATESTRUCT)lParam;
+            HWND hBtn1;
+            //普通按钮BS_PUSHBUTTON
+            hBtn1 = CreateWindow(TEXT("BUTTON"), //类名
+                TEXT("hello"), //标题名称
+                WS_CHILD/*创建子窗口*/|WS_VISIBLE/*可见*/|BS_PUSHBUTTON, //按钮样式,第三个为按钮风格
+                300,300, //位置
+                100,60, //宽度，高度
+                hwnd, //父窗口句柄
+                (HMENU)1, //菜单句柄 控件id号 唯一
+                ((LPCREATESTRUCT)lParam)->hInstance, //实例句柄
+                NULL);
+            
+            //单选按钮
+
+            HWND hBtn2 = CreateWindow(TEXT("BUTTON"), //类名
+                TEXT("hi"), //标题名称
+                WS_CHILD/*创建子窗口*/|WS_VISIBLE/*可见*/|BS_AUTORADIOBUTTON, //按钮样式,第三个为按钮风格
+                200,300, //位置
+                100,60, //宽度，高度
+                hwnd, //父窗口句柄
+                (HMENU)2, //菜单句柄 控件id号 唯一
+                ((LPCREATESTRUCT)lParam)->hInstance, //实例句柄
+                NULL);
+
+            //复选按钮
+            HWND hBtn3 = CreateWindow(TEXT("BUTTON"), //类名
+                TEXT("hi"), //标题名称
+                WS_CHILD/*创建子窗口*/|WS_VISIBLE/*可见*/|BS_AUTOCHECKBOX, //按钮样式,第三个为按钮风格
+                400,300, //位置
+                100,60, //宽度，高度
+                hwnd, //父窗口句柄
+                (HMENU)3, //菜单句柄 控件id号 唯一
+                ((LPCREATESTRUCT)lParam)->hInstance, //实例句柄
+                NULL);
+            break;
+        }
         case WM_CLOSE:
             DestroyWindow(hwnd);
             OutputDebugStr("WM_CLOSE消息\n");
@@ -87,6 +128,37 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
+        case WM_COMMAND:
+        {
+            //子窗口向父窗口传递消息时使用
+            switch(LOWORD(wParam)) //wParam低字节代表空间id
+            {
+                case 1:
+                {
+                    //wParam高字节代表控件消息类型
+                    if(HIWORD(wParam)==BN_CLICKED) //按下按钮的消息
+                    {
+                        MessageBox(NULL,TEXT("button"),TEXT("oops"),MB_OK);
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    if(HIWORD(wParam)==BN_CLICKED)
+                    {
+                        //单击后的处理
+                    }
+                    //查看按钮状态
+                    if(SendMessage((HWND)lParam,BM_GETCHECK,0,0)==BST_CHECKED)
+                    {
+                        MessageBox(hwnd,TEXT("checked!"),TEXT("tips"),MB_OK);
+                    }
+                }
+                default:
+                break;
+            }
+            break;
+        }
         case WM_RBUTTONDBLCLK:
             MessageBox(hwnd, TEXT("right click"), TEXT("message"), MB_OK);
             return 0;
